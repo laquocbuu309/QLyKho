c28749ebd8d17bf579f63fdcf7aaad5e5a44c88d
laquocbuu309 <buulaquoc@gmail.com>
2020-03-25 17:12:20 +0700

    Upfile

---
+++ QLyKho/QLyKho/ViewModel/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QLyKho.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public bool isLoaded = false;
+        public MainViewModel()
+        {
+            if (!isLoaded)
+            {
+                isLoaded = true;
+                LoginWindows login = new LoginWindows();
+                login.ShowDialog();
+
+            }
+            
+        }
+    }
+}
