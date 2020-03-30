7f75d55fcbeafbb6ef4a62e7d90d0b824ab4ebcf
4xr0hovr <laquocbuu309@gmail.com>
2020-03-30 09:09:15 +0700

    Update-29/03/2020

--- QLyKho/QLyKho/ViewModel/MainViewModel.cs
+++ QLyKho/QLyKho/ViewModel/MainViewModel.cs
@@ -12,6 +12,9 @@ namespace QLyKho.ViewModel
     {
         public bool isLoaded = false;
         public ICommand LoadedWindowCommand { get; set; }
+        public ICommand UnitCommand { get; set; }
+        public ICommand SupplierCommand { get; set; }
+        public ICommand CustomerCommand { get; set; }
         public MainViewModel()
         {
             LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
@@ -19,6 +22,27 @@ namespace QLyKho.ViewModel
                 isLoaded = true;
                 LoginWindows loginWindow = new LoginWindows();
                 loginWindow.ShowDialog();
+            }
+            );
+
+            UnitCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                UnitWindow wd = new UnitWindow();
+                wd.ShowDialog();
+            }
+            );
+
+            SupplierCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                SupplierWindow wd = new SupplierWindow();
+                wd.ShowDialog();
+            }
+            );
+
+            CustomerCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                CustomerWindow wd = new CustomerWindow();
+                wd.ShowDialog();
             }
             );
         }
